4e1fa6720a7e23cf1c0e73c27cc6f46035019616
Marina Russ <marina@cameystudios.com>
2021-05-21 10:50:18 -0400

    adds routes

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+let path = require('path');
+
+// Routes
+module.exports = (app) => {
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+  });
+
+  app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+  });
+};
