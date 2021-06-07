bc192c92a155fbd55b7ac69eec7d4dc5c8a243f0
Marina Russ <marina@cameystudios.com>
2021-06-07 14:38:55 -0400

    corrected mongodb database name to fitnessTracker

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/msu-workout-tracker',
+    process.env.MONGODB_URI || 'mongodb://localhost/fitnessTracker',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
